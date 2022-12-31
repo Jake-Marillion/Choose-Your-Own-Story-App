4ebd8501e7d3d2429185b5712b4cd24634258db7
Jacob Marillion <110437842+Jake-Marillion@users.noreply.github.com>
2022-12-31 09:24:57 -0700

    Add files via upload

---
+++ Destini-LayoutPractice-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+        
+//        storyLabel.text = example.title
+//        choice1Button.setTitle(example.choice1, for: .normal)
+//        choice2Button.setTitle(example.choice2, for: .normal)
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        
+        storyBrain.nextStory(userAnswer)
+        
+        Timer.scheduledTimer(timeInterval: 0.15, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        storyLabel.text = storyBrain.getStoryText()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
+}
+
